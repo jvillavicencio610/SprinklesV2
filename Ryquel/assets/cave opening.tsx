e63f5f1d05e80fb2df9f518e26509862ea1b9d35
Ryquel <ryquel02@gmail.com>
2018-04-18 15:13:45 -0700

    Ryquel's changes

---
+++ Ryquel/assets/cave opening.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cave opening" tilewidth="16" tileheight="16" tilecount="24" columns="4">
+ <image source="cavernopening.png" width="64" height="96"/>
+</tileset>
